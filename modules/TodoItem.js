16ad881f98309c6f65cfd894900c81425bcff288
greatghoul <greatghoul@gmail.com>
2024-04-09 23:46:29 +0800

    add list style and input box.

--- modules/TodoItem.js
+++ modules/TodoItem.js
@@ -2,28 +2,71 @@ const TodoItem = Ractive.extend({
   template: `
     {{#with todo}}
       <li class="todo">
-        <label>
-          <input type="checkbox" name="completed" {{#completed}}checked{{/completed}} />
-          <span>{{text}}</span>
-        </label>
+        <div class="columns">
+          <label class="checkbox-column">
+            <input type="checkbox" name="completed" {{#completed}}checked{{/completed}} />
+          </label>
+          <div class="text-column">
+            <span>{{{todoText}}}</span>
+          </div>
+        </div>
       </li>
     {{/with}}
   `,
   css: `
     .todo {
       list-style-type: none;
-      margin: 0 1px;
-      padding: 2px 5px;
+      margin-bottom: 2px;
+      background-color: #e5f9ff;
+      transition: background-color 0.3s ease;
     }
 
     .todo:hover {
-      background-color: #fffed5;
+      background-color: #d0ebff;
+    }
+
+    .columns {
+      display: flex;
+      align-items: left;
+      gap: 0.3rem;
+    }
+
+    .checkbox-column {
+      width: 40px;
+      min-width: 40px;
+      background-color: lightblue;
+      text-align: center;
+      padding: 5px 3px;
+    }
+
+    .checkbox-column input {
+      width: 14px;
+      height: 14px;
+    }
+
+    .text-column {
+      flex-grow: 1;
+      padding: 5px 3px;
+    }
+
+    .text-column span {
+      word-wrap: break-word;
+      white-space: pre-wrap;
+      overflow-wrap: break-word;
     }
   `,
   data: function () {
     return {
       todo: null,
     };
+  },
+  computed: {
+    todoText: {
+      get() {
+        const todo = this.get('todo');
+        return marked.parseInline(todo.text);
+      },
+    },
   },
 });
 
--- pages/HomePage.js
+++ pages/HomePage.js
@@ -9,6 +9,7 @@ const HomePage = Ractive.extend({
   },
   template: `
     <h1>Todos</h1>
+    <input type="text" class="input-new" on-keydown='handleInput' />
     <ul class="todo-list">
       {{#each todos as todo}}
         <TodoItem todo={{todo}} />
@@ -19,16 +20,28 @@ const HomePage = Ractive.extend({
     .todo-list {
       padding: 0;
     }
+    .input-new {
+      width: 100%;
+      box-sizing: border-box;
+      max-width: 100%;
+    }
   `,
   data: function () {
     return {
       todos: [],
+      inputValue: '',
     };
   },
   oninit: function () {
     taskService
       .getUserTasks({ type: "todos" })
       .then(todos => this.set({ todos }))
+  },
+  handleInput: function (ctx, e) {
+    if (e.key === 'Enter') {
+      console.log(this.get('inputValue'));
+      this.set('inputValue', '');
+    }
   },
 });
 
