5f12fc3626de9589cfb7a0a01ac550760a67de40
greatghoul <greatghoul@gmail.com>
2024-07-24 23:15:53 +0800

    style the nav item button.

--- modules/NavItem.js
+++ modules/NavItem.js
@@ -13,6 +13,15 @@ const NavItem = Ractive.extend({
       display: block;
       width: 100%;
       line-height: 2;
+      border: 0;
+      cursor: pointer;
+      background-color: #FAEDCD;
+    }
+    .nav-item:hover {
+      background-color: #F0E6D3;
+    }
+    .nav-item:active {
+      background-color: #E6DAC2;
     }
   `
 });
